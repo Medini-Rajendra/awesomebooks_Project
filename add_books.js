1c4af478d83801bb0ba3e26550c22780494c7593
Luis Fernando Jimenez <lu.jim@protonmail.com>
2021-10-25 13:53:04 -0400

    Generate  book list dynamically on the homepage

--- add_books.js
+++ add_books.js
@@ -1,10 +1,36 @@
-let booksList=[];
+let booksList = [];
+const listSection = document.getElementById('list-section');
 
+// Helper functions
 function createBook(title, author) {
-    let addObject={title,author};
-    booksList.push(addObject);
+  let addObject = { title, author };
+  booksList.push(addObject);
 }
 
-function removeBook(booksList,title,author) {
-    booksList=booksList.filter(book=>book.title!==title && book.author!==author)
+function deleteBook(title, author) {
+  booksList = booksList.filter(
+    (book) => book.title !== title && book.author !== author,
+  );
 }
+
+const createNode = (type, nodeClass) => {
+  const node = document.createElement(type);
+  if (nodeClass) node.className = nodeClass;
+  return node;
+};
+
+const showBooks = (array) => {
+  const listDiv = createNode('div', 'list-div');
+  listSection.appendChild(listDiv);
+  const listUl = createNode('ul', 'list-ul list-group');
+  listSection.appendChild(listUl);
+  array.forEach((book) => {
+    const listItem = createNode('li', 'list-group-item');
+    listUl.appendChild(listItem);
+    listItem.innerHTML = `${book.title} - ${book.author}`;
+  });
+};
+
+createBook('abc', 'def');
+createBook('123', '456');
+showBooks(booksList);

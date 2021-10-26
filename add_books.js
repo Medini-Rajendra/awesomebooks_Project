87221cc52a1653e3770b16cffe6d4bc86a39d962
Luis Fernando Jimenez <lu.jim@protonmail.com>
2021-10-26 16:26:38 -0400

    Refactor booklist with classes

--- add_books.js
+++ add_books.js
@@ -1,84 +1,90 @@
-const listSection = document.getElementById('list-section');
-
-// Helper functions
-const getBook = () => {
-  let result;
-  if (localStorage.getItem('list') === null) {
-    result = [];
-  } else {
-    result = JSON.parse(localStorage.getItem('list'));
-  }
-  return result;
-};
-let booksList = getBook();
-
-const createBook = (title, author) => {
-  const addObject = [{ title, author }];
-  booksList = booksList.concat(addObject);
-};
-
-const deleteBook = (title, author) => {
-  booksList = booksList.filter(
-    (book) => book.title !== title && book.author !== author,
-  );
-};
-
+// Helper function
 const createNode = (type, nodeClass) => {
   const node = document.createElement(type);
   if (nodeClass) node.className = nodeClass;
   return node;
 };
 
-const showBooks = (array) => {
-  const listDiv = createNode('div', 'list-div');
-  listSection.appendChild(listDiv);
-  array.forEach((book) => {
-    const listEntry = createNode('div');
-    listDiv.appendChild(listEntry);
-    listEntry.id = `book-${array.indexOf(book)}`;
-    const listItem = createNode('p', '');
-    listEntry.appendChild(listItem);
-    listItem.innerHTML = `${book.title} <br> ${book.author}`;
-    const listButton = createNode('button', 'list-button');
-    listEntry.appendChild(listButton);
-    listButton.innerHTML = 'Remove';
-    listButton.setAttribute('class', 'buttonsize');
-    listButton.id = `button-${array.indexOf(book)}`;
-    const bookIndex = `${array.indexOf(book)}`;
-    listButton.setAttribute('onclick', `removeBook(${bookIndex})`);
-    const listhr = createNode('hr');
-    listEntry.appendChild(listhr);
-  });
-};
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
 
-const addTitleAuthor = () => {
-  const addTitle = document.getElementById('title').value;
-  const addAuthor = document.getElementById('author').value;
-  createBook(addTitle, addAuthor);
-  const listContainer = document.querySelector('#list-section');
-  const oldList = document.querySelector('.list-div');
-  listContainer.removeChild(oldList);
-  localStorage.setItem('list', JSON.stringify(booksList));
-  showBooks(booksList);
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-};
+class bookList {
+  static showBooks() {
+    const books = Save.getBooks();
+    books.forEach((book) => bookList.addBook(book));
+  }
+  static addBook(book) {
+    const list = document.getElementById('list');
+    const entry = createNode('div', `book`);
 
-const removeBook = (bookIndex) => {
-  const findBook = document.querySelector(`#book-${bookIndex}`);
-  deleteBook(booksList[bookIndex].title, booksList[bookIndex].author);
-  findBook.remove();
-  const listContainer = document.querySelector('#list-section');
-  const oldList = document.querySelector('.list-div');
-  listContainer.removeChild(oldList);
-  localStorage.setItem('list', JSON.stringify(booksList));
-  showBooks(booksList);
-};
-showBooks(booksList);
-module.exports = {
-  addTitleAuthor,
-  removeBook,
-  rules: {
-    'linebreak-style': 0,
-  },
-};
+    entry.innerHTML = `
+    <p class="${book.title}" id="${book.author}"> ${book.title} - ${book.author} <a ref="" class="btn btn-danger btn-sm delete">X</a> </p>
+    `;
+    list.appendChild(entry);
+  }
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+}
+
+class Save {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('list') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('list'));
+    }
+    return books;
+  }
+  static addBook(book) {
+    let books = Save.getBooks();
+    let newBook = [book];
+    books = books.concat(newBook);
+    localStorage.setItem('list', JSON.stringify(books));
+  }
+  static removeBook(title, author) {
+    let books = Save.getBooks();
+    books = books.filter(
+      (book) => book.title !== title && book.author !== author,
+    );
+    localStorage.setItem('list', JSON.stringify(books));
+  }
+}
+
+// Show books
+document.addEventListener('DOMContentLoaded', bookList.showBooks);
+
+// Add Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+
+  //Validate
+  if (title == '' || author == '') {
+    alert('Please fill in all fields');
+  } else {
+    const book = new Book(title, author);
+    bookList.addBook(book);
+    Save.addBook(book);
+    bookList.clearFields();
+  }
+});
+
+// Remove Book
+document.querySelector('#list').addEventListener('click', (e) => {
+  bookList.deleteBook(e.target);
+  Save.removeBook(e.target.parentElement.class, e.target.parentElement.id);
+});
